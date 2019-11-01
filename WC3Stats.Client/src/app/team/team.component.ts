81a06d03b4fe4f43611f669fb3cef67d4cb1f65d
Alexander Tank <Alexander.Tank@comparex.com>
2019-11-01 16:14:01 +0100

    show all teams in UI

---
+++ WC3Stats.Client/src/app/team/team.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Player } from '../app.component';
+
+@Component({
+  selector: 'app-team',
+  templateUrl: './team.component.html',
+  styleUrls: ['./team.component.css']
+})
+export class TeamComponent implements OnInit {
+
+  @Input() team: Player[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
